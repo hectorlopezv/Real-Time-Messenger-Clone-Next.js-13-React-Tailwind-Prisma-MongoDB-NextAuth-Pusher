7df8a34305d45b62274c72571188460344f38824
hector lopez <hectorvmlopez@gmail.com>
2023-06-18 23:18:22 -0500

    adding form component and individual component using react hook form and clsx, tailwind

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import Button from "@/app/components/inputs/Button";
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+type Props = {};
+type Variant = "LOGIN" | "REGISTER";
+
+export default function AuthForm({}: Props) {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setisLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setisLoading(true);
+
+    //sign user
+    if (variant === "LOGIN") {
+    }
+
+    //next auth signin
+    if (variant === "REGISTER") {
+    }
+  };
+  const socialAction = (action: string) => {
+    setisLoading(true);
+    ///social auth signIn
+  };
+  const toogleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" ? (
+            <Input label="Name" register={register} id="name" errors={errors} />
+          ) : null}
+          <Input
+            label="Email"
+            register={register}
+            type="email"
+            id="email"
+            errors={errors}
+          />
+          <Input
+            label="Password"
+            type="password"
+            register={register}
+            id="password"
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or Continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toogleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Sign in"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import AuthForm from "./components/AuthForm";
 
 type Props = {};
 
@@ -21,6 +22,7 @@ export default function Home({}: Props) {
           Sign in into your account
         </h2>
       </div>
+      <AuthForm />
     </div>
   );
 }
